cba4d041c5adbfc5b318d3982291edf6b76017d8
4f48 <0x4f48@proton.me>
2025-06-19 18:12:31 +0200

    cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,15 @@
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
+	adapter: cloudflare({
+		imageService: "cloudflare",
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 	experimental: {
 		fonts: [
 			{
@@ -16,5 +23,12 @@ export default defineConfig({
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: import.meta.env.PROD
+				? {
+						"react-dom/server": "react-dom/server.edge",
+					}
+				: undefined,
+		},
 	},
 });
