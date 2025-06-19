faaec400a0c0f5cecb87abac58ea93a626817f70
4f48 <0x4f48@proton.me>
2025-06-19 17:25:45 +0200

    add inter font

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,20 @@
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
+	experimental: {
+		fonts: [
+			{
+				cssVariable: "--font-inter",
+				featureSettings: "'liga' 1, 'calt' 1",
+				name: "Inter",
+				provider: fontProviders.bunny(),
+			},
+		],
+	},
+	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	integrations: [react()],
 });
