2f8687878622a6932e22a6d4e6213a944bfe012c
4f48 <0x4f48@proton.me>
2025-06-19 22:24:22 +0200

    cloudflare is trash

--- astro.config.ts
+++ astro.config.ts
@@ -1,38 +1,37 @@
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
-	adapter: cloudflare({
-		imageService: "cloudflare",
-	}),
-	site: "https://soundstash.pirger.eu",
-	experimental: {
-		fonts: [
-			{
-				cssVariable: "--font-inter",
-				featureSettings: "'liga' 1, 'calt' 1",
-				name: "Inter",
-				provider: fontProviders.bunny(),
-			},
-		],
-	},
-	integrations: [react()],
-	output: "server",
-	vite: {
-		build: {
-			rollupOptions: {
-				maxParallelFileOps: 200,
-			},
-		},
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: import.meta.env.PROD
-				? {
-						"react-dom/server": "react-dom/server.edge",
-					}
-				: undefined,
-		},
-	},
+    adapter: vercel(),
+    site: "https://soundstash.pirger.eu",
+    experimental: {
+        fonts: [
+            {
+                cssVariable: "--font-inter",
+                featureSettings: "'liga' 1, 'calt' 1",
+                name: "Inter",
+                provider: fontProviders.bunny(),
+            },
+        ],
+    },
+    integrations: [react()],
+    output: "server",
+    vite: {
+        build: {
+            rollupOptions: {
+                maxParallelFileOps: 200,
+            },
+        },
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: import.meta.env.PROD
+                ? {
+                        "react-dom/server": "react-dom/server.edge",
+                    }
+                : undefined,
+        },
+    },
 });
