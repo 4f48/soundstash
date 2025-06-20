0f37cd513bdc3cb5f0f7b679d5aac4940ba8b444
4f48 <0x4f48@proton.me>
2025-06-20 18:43:48 +0200

    idk

--- src/pages/api/upload/finalize.ts
+++ src/pages/api/upload/finalize.ts
@@ -5,13 +5,11 @@ import type { APIRoute } from "astro";
 export const POST: APIRoute = async (ctx) => {
 	const metadata: App.Track = JSON.parse(await ctx.request.json());
 	console.debug(JSON.stringify(metadata));
-	const userId = ctx.locals.user?.id;
-	if (!userId) throw Error("user is not defined");
 	await db.insert(track).values({
 		artist: metadata.artist,
 		blob: metadata.blob!,
 		id: metadata.id,
-		owner: userId,
+		owner: ctx.locals.user?.id!,
 		size: metadata.size,
 		title: metadata.title,
 	});
