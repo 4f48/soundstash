1b8d1b0268e85750168f3e15d72c021f120b787d
4f48 <0x4f48@proton.me>
2025-06-21 15:20:15 +0200

    authenticate upload route

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -4,4 +4,10 @@ export const authClient = createAuthClient({
 	baseURL: import.meta.env.PROD
 		? import.meta.env.SITE
 		: "http://localhost:4321",
+	fetchOptions: {
+		onSuccess: (ctx) => {
+			const authToken = ctx.response.headers.get("set-auth-token");
+			if (authToken) localStorage.setItem("bearer_token", authToken);
+		},
+	},
 });
