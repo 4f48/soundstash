568c3b74a9a79f2446ce88f3c65a271ba7530a28
4f48 <0x4f48@proton.me>
2025-06-25 16:18:19 +0200

    neon serverless, fix query spam

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,4 @@
 import * as schema from "@/lib/schema";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 export const db = drizzle(import.meta.env.DATABASE_URL, { schema });
