52e3b9c49727bdd7d793366fbfd42135b365b6dd
4f48 <0x4f48@proton.me>
2025-07-04 19:32:29 +0200

    card

---
+++ src/components/card/index.ts
@@ -0,0 +1,4 @@
+export { default as Card } from "./card.astro";
+export { default as CardHeader } from "./card-header.astro";
+export { default as CardTitle } from "./card-title.astro";
+export { default as CardDescription } from "./card-description.astro";
