5a13edae35943f08201e9c51eedbd81a1d1ccf57
4f48 <0x4f48@proton.me>
2025-07-04 18:07:18 +0200

    proper mobile header

--- src/components/header/link.svelte
+++ src/components/header/link.svelte
@@ -15,7 +15,7 @@
 <NavigationMenu.Item>
 	<NavigationMenu.Link
 		class={cn(
-			"focus-visible:ring-offset-primary/20 text-nowrap focus-visible:ring-muted inline-flex items-center justify-center rounded-sm text-sm font-medium duration-150 focus-visible:outline-none focus-visible:ring focus-visible:ring-offset-2 disabled:opacity-75 text-foreground hover:bg-primary/5 h-8 gap-1.5 px-3",
+			"focus-visible:ring-offset-primary/20 w-full sm:bg-transparent bg-primary/5 sm:w-fit text-nowrap focus-visible:ring-muted inline-flex items-center justify-center rounded-sm text-sm font-medium duration-150 focus-visible:outline-none focus-visible:ring focus-visible:ring-offset-2 disabled:opacity-75 text-foreground hover:bg-primary/5 h-8 gap-1.5 px-3",
 			className
 		)}
 		{href}>{@render children()}</NavigationMenu.Link
