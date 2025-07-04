fdd68a09f9af5b2bdc5029c8d65229130badfe5f
4f48 <0x4f48@proton.me>
2025-07-04 22:16:16 +0200

    sign up

--- src/components/forms/signin.svelte
+++ src/components/forms/signin.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import Button from "./button.svelte";
-	import Input from "./input.svelte";
-	import Label from "./label.svelte";
+	import Formfield from "./formfield.svelte";
+	import Button from "@/components/button.svelte";
 	import { newAuthClient as authClient } from "@/lib/auth/client";
-	import { reporter, ValidationMessage } from "@felte/reporter-svelte";
+	import { reporter } from "@felte/reporter-svelte";
 	import { validator } from "@felte/validator-zod";
 	import { navigate } from "astro:transitions/client";
 	import { createForm } from "felte";
@@ -39,28 +38,8 @@
 </script>
 
 <form class="flex flex-col gap-2" use:form>
-	<div class="grid">
-		<div class="flex items-center">
-			<Label for="email">Email</Label>
-			<ValidationMessage for="email" let:messages={message}>
-				<p class="text-destructive flex-1 text-end text-sm font-medium">
-					{message && message[0]}
-				</p>
-			</ValidationMessage>
-		</div>
-		<Input id="email" name="email" />
-	</div>
-	<div class="grid">
-		<div class="flex items-center">
-			<Label for="password">Password</Label>
-			<ValidationMessage for="password" let:messages={message}>
-				<p class="text-destructive flex-1 text-end text-sm font-medium">
-					{message && message[0]}
-				</p>
-			</ValidationMessage>
-		</div>
-		<Input id="password" type="password" name="password" />
-	</div>
+	<Formfield name="Email" />
+	<Formfield name="Password" type="password" />
 	<Button loading={$isSubmitting} type="submit">Sign in</Button>
 </form>
 <span class="text-fg1 text-center text-sm [&_a]:underline">
