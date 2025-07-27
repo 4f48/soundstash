9c1ca9c117b62deaffa434018e6dbfeea518ea93
4f48 <0x4f48@proton.me>
2025-07-27 20:34:14 +0200

    style tost + fix it getting stuck

---
+++ src/components/toaster.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { CheckCircle, ExclamationTriangle, Icon } from "svelte-hero-icons";
+	import { Toaster } from "svelte-sonner";
+</script>
+
+<Toaster
+	swipeDirections={["right", "bottom"]}
+	toastOptions={{
+		unstyled: true,
+		class: "flex items-center font-medium text-sm p-4 gap-3 rounded-sm border",
+		classes: {
+			error: "bg-[#fb4934] border-[#cc241d]  text-bg0",
+			success: "bg-[#b8bb26] border-[#98971a] text-fg",
+		},
+	}}
+>
+	{#snippet successIcon()}
+		<Icon class="size-5" src={CheckCircle} solid />
+	{/snippet}
+	{#snippet errorIcon()}
+		<Icon class="size-5" src={ExclamationTriangle} solid />
+	{/snippet}
+</Toaster>
