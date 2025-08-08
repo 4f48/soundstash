929c14d7cf0bce5912b6848748b19a32bf7d1443
Oliver Pirger <oliver@pirger.eu>
2025-08-08 16:44:34 +0200

    download tracks

--- src/components/storage/actions.svelte
+++ src/components/storage/actions.svelte
@@ -6,9 +6,9 @@
 	import {
 		EllipsisHorizontal,
 		Icon,
-		ArchiveBox,
 		Pencil,
 		Trash,
+		FolderArrowDown,
 	} from "svelte-hero-icons";
 
 	interface Props {
@@ -17,6 +17,34 @@
 	const { track }: Props = $props();
 
 	let open = $state(false);
+
+	async function download() {
+		try {
+			const urlResult = await fetch(`/api/track/${track.id}`);
+			if (!urlResult.ok) return;
+			const url = await urlResult.text();
+			if (!url) return;
+
+			const fileResult = await fetch(url);
+			if (!fileResult.ok) return;
+
+			const blob = await fileResult.blob();
+			const a = document.createElement("a");
+
+			const artist = track.artist ? `${track.artist} - ` : "";
+			const title = track.title ?? track.id;
+			a.download = `${artist}${title}`.replace(/[\\/:*?"<>|]+/g, "_");
+			a.href = URL.createObjectURL(blob);
+			document.body.appendChild(a);
+			a.click();
+			a.remove();
+			URL.revokeObjectURL(a.href);
+		} catch (e) {
+			if (e instanceof Error)
+				console.error(`failed to download ${track.title}: ${e.message}`);
+			else console.error("something went wrong: ", e);
+		}
+	}
 </script>
 
 <Delete bind:open {track} />
@@ -31,7 +59,9 @@
 			class="bg-bg1 text-fg border-bg2 flex flex-col rounded-sm border p-1"
 		>
 			<Item disabled><Icon src={Pencil} solid />Edit</Item>
-			<Item disabled><Icon src={ArchiveBox} solid />Compress</Item>
+			<Item onclick={download}
+				><Icon src={FolderArrowDown} solid />Download</Item
+			>
 			<Item onclick={() => (open = true)}><Icon src={Trash} solid />Delete</Item
 			>
 		</DropdownMenu.Content>
